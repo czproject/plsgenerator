4eb9b1c5c54a0fab2e8fca6948c85d3902de0961
Jan Pecha <janpecha@email.cz>
2013-07-09 20:30:41 +0200

    init commit
    Signed-off-by: Jan Pecha <janpecha@email.cz>

---
+++ readme.md
@@ -0,0 +1,41 @@
+PlsGenerator
+============
+
+``` php
+<?php
+$playlist = new Cz\PlsGenerator;
+
+// add track (only $filename is required paramater)
+$playlist->addTrack($filename, $title, $length);
+
+// generate (print to page)
+echo $playlist; // or echo $playlist->generate();
+
+// or save playlist into file
+$playlist->save('playlist.pls');
+```
+
+
+Dynamic online playlist
+-----------------------
+
+``` php
+<?php
+header('Content-type: ' . Cz\PlsGenerator::MIME_TYPE);
+header('Content-Disposition: attachment; filename="music.pls"');
+
+$playlist = new Cz\PlsGenerator;
+
+// add tracks
+$playlist->addTrack('track1.mp3', 'Oasis - The Masterplan');
+$playlist->addTrack('track2.mp3', 'The Cranberries - Zombie');
+
+// send to browser
+echo $playlist;
+```
+
+-------------------------------------------------
+
+License: New BSD License
+ Author: Jan Pecha, http://janpecha.iunas.cz/
+
---
+++ src/PlsGenerator.php
@@ -0,0 +1,127 @@
+<?php
+	/** PLS Playlist Generator
+	 * 
+	 * @author		Jan Pecha, <janpecha@email.cz>
+	 * @version		2012-10-14-2
+	 * @license		New BSD License
+	 */
+	
+	namespace Cz;
+	
+	class PlsGenerator
+	{
+		const MIME_TYPE = 'audio/x-scpls';
+		
+		/** @var  string[] */
+		private $tracks = array();
+		
+		
+		
+		/**
+		 * @param	string|array
+		 * @param	string
+		 * @param	int
+		 * @throw	Exception
+		 * @return	$this
+		 */
+		public function addTrack($file, $title = NULL, $length = NULL)
+		{
+			if(is_array($file))
+			{
+				$title = (isset($file['title'])) ? $file['title'] : 'Unknow';
+				$length = (isset($file['length'])) ? $file['length'] : FALSE;
+				
+				if(isset($file['file']))
+				{
+					$file = $file['file'];
+				}
+				else
+				{
+					throw new \Exception('Filename is undefined.');
+				}
+			}
+			
+			if($length === NULL || $length === FALSE)
+			{
+				$length = '-1'; // indefinite
+			}
+			else
+			{
+				$length = (int)$length;
+			}
+			
+			$this->tracks[] = array(
+				'file' => (string)$file,
+				'title' => $title,
+				'length' => $length,
+			);
+			
+			return $this;
+		}
+		
+		
+		
+		/**
+		 * @param	string[]
+		 * @return	$this
+		 */
+		public function addTracks(array $tracks)
+		{
+			foreach($tracks as $track)
+			{
+				$this->addTracks($track);
+			}
+			
+			return $this;
+		}
+		
+		
+		
+		/**
+		 * @param	string
+		 * @return	int|FALSE
+		 */
+		public function save($filename)
+		{
+			return file_put_contents($filename, $this->generate());
+		}
+		
+		
+		
+		/**
+		 * @return	string
+		 */
+		public function generate()
+		{
+			// Header
+			$data = "[playlist]\n\n";
+			$number = 0;
+			
+			// Tracks
+			foreach($this->tracks as $i => $track)
+			{
+				$data .= 'File' . ($i + 1) . "={$track['file']}\n";
+				$data .= 'Title' . ($i + 1) . "={$track['title']}\n";
+				$data .= 'Length' . ($i + 1) . "={$track['length']}\n\n";
+				
+				$number++;
+			}
+			
+			// Footer
+			$data .= "\nNumberOfEntries=$number\n";
+			$data .= "Version=2";
+			
+			return $data;
+		}
+		
+		
+		
+		/**
+		 * @return	string
+		 */
+		public function __toString()
+		{
+			return $this->generate();
+		}
+	}
+
